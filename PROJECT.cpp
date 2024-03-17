73b1d72b058712bb2e15efbce4de1a160454d43d
Mohamed Ahmed <mohamed.ahmed.abdelghani6@gmail.com>
2024-03-17 02:15:42 +0200

    init

---
+++ PROJECT.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+const int maxusers = 100;
+const int maxcars = 100;
+
+
+struct Customer {
+    string name;
+    string mobileNum;
+    string address;
+};
+
+struct Car {
+    string brand;
+    string model;
+    string color;
+    float distanceTraveled;
+    bool available;
+};
+
+
+void addCar();
+void updateCar();
+void removeCar();
+void listCars();
+void checkCar();
+void rentCar();
+void login();
+
+int main() {
+
+    Customer customers[maxusers]{
+
+
+
+
+    };
+    Car cars[maxcars]{
+
+
+
+
+
+    };
+
+
+
+    int numCustomers = 0;
+    int numCars = 0;
+    login();
+
+
+    int ans;
+    do {
+
+        cout << "\n----------------------\n";
+        cout << "Main Menu:\n";
+        cout << "1. Add Car\n";
+        cout << "2. Update Car Details\n";
+        cout << "3. Remove Car\n";
+        cout << "4. List Cars\n";
+        cout << "5. Check Car Availability\n";
+        cout << "6. Rent Car\n";
+        cout << "7. Logout\n";
+        cout << "\n----------------------\n";
+        cout << "Enter your choice: ";
+        cin >> ans;
+        switch (ans) {
+            case 1:
+                addCar();
+                break;
+            case 2:
+                updateCar();
+                break;
+            case 3:
+                removeCar();
+                break;
+            case 4:
+                listCars();
+                break;
+            case 5:
+                checkCar();
+                break;
+            case 6:
+                rentCar();
+                break;
+            case 7:
+                cout << "Logged out successfully.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (ans != 7);
+    return 0;
+}
+
+void addCar() {
+  
+}
+
+void updateCar() {
+    
+}
+
+void removeCar() {
+   
+}
+
+void listCars() {
+    
+}
+
+void checkCar() {
+    
+}
+
+void rentCar() {
+    
+}
+
+void login() {
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Car rental system 
+
+<h3>SP Project</h3>---------------
+
+The system is for managing car rental, allowing users to rent, add or remove a car and updating car details.
+
+The project contains 7 functions :
+
+1 \ addCar
+
+2 \ updateCar
+
+3 \ removeCar
+
+4 \ listCars
+
+5 \ checkCar
+
+6 \ rentCar
+
+7 \ login
+
+
+Structs:
+-
+ (Customer) and (Car) are defined to hold the data of customers and cars.
+
+
+Arrays:
+-
+ (customers) and (cars) are used to store customer and car records.
+
+functions:
+-
+You'll need to implement the functionalities such as adding cars, renting cars, and the login system.
+
+
+
+good luck :)
