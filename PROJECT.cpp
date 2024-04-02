2b7830ebb9d3a32014da757662a694375fca3552
Mohamed Ahmed <mohamed.ahmed.abdelghani6@gmail.com>
2024-04-02 22:10:22 +0200

    editing some functions

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-//// ----- Constants ------
+// ----- Constants ------
 
 const int MAX_USERS = 100;
 const int MAX_CARS = 100;
@@ -38,12 +38,12 @@ Customer customers[MAX_USERS]{};
 
 // ----- Functions ------
 
-void addCar();
+void addCar(int& m);
 void updateCar();
 void removeCar(int* numCars, int to_remove);
 void listCars(int n);
 bool checkCar();
-void rentCar();
+void rentCar(int x);
 void login();
 
 int main() {
@@ -71,14 +71,14 @@ int main() {
         cin >> ans;
         switch (ans) {
         case 1:
-            addCar();
+            addCar(numCars);
             break;
         case 2:
             updateCar();
             break;
-        case 3: // loay
+        case 3:
             int to_remove;
-            cout << "Enter number of car want to remove: ";
+            cout << "Enter the number of the car you want to remove: ";
             cin >> to_remove;
             removeCar(&numCars, to_remove);
             break;
@@ -96,7 +96,7 @@ int main() {
             }
             break;
         case 6:
-            rentCar();
+            rentCar(numCars);
             break;
         case 7:
             cout << "Logged out successfully.\n";
@@ -112,14 +112,35 @@ int main() {
 
 
 
-void addCar() {
-    //TODO:muhammad
-}
+void addCar(int& m) {//TODO:muhammad
 
+    cars[m].carnum = m + 1; // index of new car is m , and m is the number of cars in system
+
+    cout << "Enter car's Brand: \n";
+    cin >> cars[m].brand;
+
+    cout << "Enter car's Model \n";
+    cin >> cars[m].model;
+
+    cout << "Enter car's Color \n";
+    cin >> cars[m].color;
+
+    cout << "Enter car's Distance Traveled \n";
+    cin >> cars[m].distanceTraveled;
+
+    cars[m].available = true;
+
+    cout << "The New Car's Number is: " << cars[m].carnum << endl;
+    m++;
+    cout << "Car Added Successfully!\n";
+    cout << "\t-----\n";
+
+}
+// done ... <3
 
 void updateCar() {
     int choice;
-    cout << "Enter the number of the car you want to update its details :" << endl;
+    cout << "Enter the number of the car you want to update its details : ";
     cin >> choice;
     if (choice >= 1 && choice <= MAX_CARS) {
         int carindex = choice - 1;
@@ -139,41 +160,48 @@ void updateCar() {
 }
 //done ... <3
 
-void removeCar(int* numCars, int to_remove) { //TODO: loay
-    for (int i = 1 ; i < *numCars; i++)
+void removeCar(int* numCars, int to_remove) { //TODO: loay && will be updated removing by number of listing car not by carnum
+    bool isfound = false;
+
+    for (int i = 0; i < *numCars; i++)
     {
-        if (cars[i-1].carnum == to_remove) //from the removed: every one equal to the next until end
+        if (cars[i].carnum == to_remove)
         {
-            for (int j = i; j < *numCars; j++)
-            {
-                cars[j-1].carnum = cars[j].carnum;
-                cars[j-1].available = cars[j].available;
-                cars[j-1].brand = cars[j].brand;
-                cars[j-1].color = cars[j].color;
-                cars[j-1].distanceTraveled = cars[j].distanceTraveled;
-                cars[j-1].model = cars[j].model;
-            }
+            isfound = true;
+            cars[i] = cars[*numCars - 1]; //just swap
+            (*numCars)--; //to remove from couter the last one
+            cout << "The car has been removed successfully !! \n";
+            break;
         }
     }
-    *numCars--; //to remove from couter the last one
-    
-}
 
-
-void listCars(int n) {
-    //TODO:mohamedAhmed
-    cout << "\nThe list of Cars :\n";
-    cout << "\n";
-    for (int i = 0; i < n; i++) {
-
-        cout << "car number " << cars[i].carnum << "\n";
-        cout << "brand : " << cars[i].brand << "\n";
-        cout << "model : " << cars[i].model << "\n";
-        cout << "Color : " << cars[i].color << "\n";
-        cout << "Traveld Distance : " << cars[i].distanceTraveled << "\n";
-
+    if (!isfound) {
+        cout << "the car is not found\n";
     }
 
+
+}
+//done ... <3
+
+void listCars(int n) { //TODO:mohamedAhmed
+    if (n > 0) {
+        cout << "\nThe list of Cars :\n";
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+
+            cout << "car number " << cars[i].carnum << "\n";
+            cout << "brand : " << cars[i].brand << "\n";
+            cout << "model : " << cars[i].model << "\n";
+            cout << "Color : " << cars[i].color << "\n";
+            cout << "Traveld Distance : " << cars[i].distanceTraveled << "\n";
+            cout << "\t-----\n";
+        }
+    }
+    else {
+
+        cout << "There are no available cars to be shown \n";
+
+    }
 }
 //done ... <3
 
@@ -193,10 +221,57 @@ bool checkCar() {
 }
 //done ... <3
 
-void rentCar() {
+void rentCar(int x)
+{
     //TODO: abdallah
-}
+    int n;
+    char z;
+    bool t = 1;
+    while (t)
+    {
+        cout << "Select the car's number : ";
+        cin >> n;
 
+        if (n <= x && n > 0) // check the number is in the range
+        {
+
+            if (cars[n - 1].available == true) // check if the car is avaliable or rented
+            {
+                cout << "\n The Car is Avaliable, You have rented it Succefully!\n\n";
+                cars[n - 1].available = false;
+                break;
+            }
+            else {
+                cout << " Already rented,";
+                bool f = 1;
+                while (f) {
+                    cout << " Do you want to choose another one? [y / n]\n";
+                    cin >> z;
+                    if (z == 'y' || z == 'Y') {
+                        bool t = 1;
+                        bool f = 0;
+                        break;
+                    }
+                    else if (z == 'n' || z == 'N') {
+                        bool t = 0;
+                        bool f = 0;
+                        return;
+                    }
+                    else {
+                        cout << "invalid choice,";
+                        continue;
+                    }
+                }
+            }
+        }
+        else {
+            cout << "Invalid Number , Please Try Again\n ";
+            continue;
+        }
+    }
+
+}
+//done ... <3 
 
 void login() {
     //TODO: mohamed gamel gdn
